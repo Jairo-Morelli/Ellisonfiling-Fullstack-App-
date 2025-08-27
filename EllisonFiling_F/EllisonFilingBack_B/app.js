8677d8e92969a078d72f6c229d729c400f671b10
jairo <jalbmorelli@hotmail.com>
2025-08-27 12:05:48 -0400

    changed directory structure again

---
+++ EllisonFiling_F/EllisonFilingBack_B/app.js
@@ -0,0 +1,19 @@
+/* 
+* 
+* App here, literally means the server. 
+* 
+*   you're creating your server application here.
+*/
+const express = require('express')
+const app = express()
+const port = 3000 
+
+
+app.get('/', (req,res) => {
+    res.send("Hello World!");
+})
+
+
+app.listen(port,() =>{ 
+    console.log(`Example app listening on port ${port}`)
+})
